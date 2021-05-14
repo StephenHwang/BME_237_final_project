b8cc3c2d769831a3187687bfcf78fb2ac96ac06a
StephenHwang <sjhwang@ucsc.edu>
2021-05-14 11:27:14 -0700

    Start gene symbol conversion.

--- src/kallisto_tcga_analysis.R
+++ src/kallisto_tcga_analysis.R
@@ -2,6 +2,11 @@ require(data.table)
 require(dplyr)
 require(plyr)
 
+library(tximport)
+library(biomaRt)
+library(stringr)
+library(forcats)
+
 home.dir <- '/home/stephen/Documents/classes/bme/237/final_project'
 setwd(home.dir)
 # download survival data
@@ -12,7 +17,7 @@ setwd(home.dir)
 # with data
 #    PCA
 #    expression boxplot
-#   
+#
 
 ds <- fread(
   file = './data/kallisto/tcga_Kallisto_est_counts',
@@ -21,7 +26,9 @@ ds <- fread(
   data.table = FALSE,
   header = TRUE
 )
-ds_samples <- colnames(ds) 
+rownames(ds) <- ds$sample
+ds <- ds[-1]
+ds_samples <- colnames(ds)
 
 meta.data <- fread(
   file = './data/kallisto/Survival_SupplementalTable_S1_20171025_xena_sp',
@@ -78,19 +85,80 @@ rownames(d) <- rownames(tmp.files[[1]])
 valid.samples <- intersect(sample_ids, colnames(d))  # in both datasets with metadata
 
 d <- d[,colnames(d) %in% valid.samples]
-ds <- ds[,colnames(ds) %in% valid.samples]
+#ds <- ds[,colnames(ds) %in% valid.samples]
+ds <- ds[, colnames(ds) %in% colnames(exp.gdc)]
 md <- meta.data[meta.data$sample %in% valid.samples, ]
 
 # save as RDS
-saveRDS(d, file = paste0(home.dir, "/data/exp_gdc.rds"))
-saveRDS(ds, file = paste0(home.dir, "/data/exp_kal.rds"))
-saveRDS(md, file = paste0(home.dir, "/data/meta_data.rds"))
+#saveRDS(d, file = paste0(home.dir, "/data/exp_gdc.rds"))
+#saveRDS(ds, file = paste0(home.dir, "/data/exp_kal.rds"))
+#saveRDS(md, file = paste0(home.dir, "/data/meta_data.rds"))
 
 # read RDS
 exp.gdc <- readRDS(paste0(home.dir, "/data/exp_gdc.rds"))
 exp.kal <- readRDS(paste0(home.dir, "/data/exp_kal.rds"))
 meta.data <- readRDS(paste0(home.dir, "/data/meta_data.rds"))
 
+# TODO:
+# check if datasets are normalized in anyway
+#    GDC: not normalized, count data
+#    Kal: est_counts (https://xenabrowser.net/datapages/?dataset=tcga_Kallisto_est_counts&host=https%3A%2F%2Ftoil.xenahubs.net&removeHub=https%3A%2F%2Fxena.treehouse.gi.ucsc.edu%3A443)
+# convert transcript ID to gene ID
+
+# get hgnc_symbol gene names
+ensembl = useMart("ensembl", dataset="hsapiens_gene_ensembl")
+enst = rownames(exp.kal)
+enst.no_version = sapply(strsplit(as.character(enst),"\\."),"[[",1)
+g.name <- getBM(attributes = c('ensembl_transcript_id', 'hgnc_symbol'), filters = 'ensembl_transcript_id', values=enst.no_version, mart=ensembl)
+head(g.name)
+
+cts <- exp.kal # exp.gdc 
+# change g.name getBM depending on dataset
+# exp.gdc uses ensembl_gene_id
+# exp.kal uses ensembl_transcript_id
+
+# convert to ensembl_transcript_id to hgnc_symbol names
+cts <- as.data.frame(cbind(cts, enst.no_version))
+cts$enst.no_version <- lvls_revalue(factor(cts$enst.no_version, levels = g.name$ensembl_transcript_id), g.name$hgnc_symbol)
+
+#cts$enst.no_version <- lvls_revalue(factor(cts$enst.no_version, levels = g.name$ensembl_transcript_id), g.name$hgnc_symbol)
+# recode into gene symbol
+# recode(unlist(lapply(tmp.files, names)), !!!gdc.files.recode)
+# ^ something like above line, with gdc.files.recode as g.name as a list
+# if works, do same for Kallisto data
+
+cts <- na.omit(cts)
+head(cts)
+
+# aggregate duplicate gene and bind to integer count
+cts.tmp <- aggregate(apply(cts[-7], 2, as.numeric), cts["enst.no_version"], sum)
+cts <- cts.tmp[-1, -1]
+cts <- apply(cts, 2, as.integer)
+rownames(cts) <- cts.tmp$enst.no_version[-1]
+head(cts)
+
+
+################################################################################
+
+enst = rownames(exp.gdc)
+enst.no_version = sapply(strsplit(as.character(enst),"\\."),"[[",1)
+g.name <- getBM(attributes = c('ensembl_gene_id', 'hgnc_symbol'), filters = 'ensembl_gene_id', values=enst.no_version, mart=ensembl)
+head(g.name)
+
+cts <- exp.gdc
+
+# convert to ensembl_gene_id to hgnc_symbol names
+cts <- as.data.frame(cbind(cts, enst.no_version))
+cts$enst.no_version <- lvls_revalue(factor(cts$enst.no_version, levels = g.name$ensembl_gene_id), g.name$hgnc_symbol)
+cts <- na.omit(cts)
+head(cts)
+
+# aggregate duplicate gene and bind to integer count
+cts.tmp <- aggregate(apply(cts[-7], 2, as.numeric), cts["enst.no_version"], sum)
+cts <- cts.tmp[-1, -1]
+cts <- apply(cts, 2, as.integer)
+rownames(cts) <- cts.tmp$enst.no_version[-1]
+head(cts)
 
 
 
