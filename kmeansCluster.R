06f347c8d364c16f57af4a28b5368be0fde3bee3
StephenHwang <sjhwang@ucsc.edu>
2021-05-28 15:09:04 -0700

    Intersect significant cibersort datasets.

--- kmeansCluster.R
+++ kmeansCluster.R
@@ -2,45 +2,70 @@
 setwd("/home/tmpereir/final")
 # install.packages("factoextra")
 library(factoextra)
+library(data.table)
 
-# Loading and preprocessing GDC data
-gdc_data <- read.table(file = 'cb_gdc_results.txt', sep = '\t', header = TRUE)
 
-# Only keep samples with p-value <= 0.05
-gdc_data <- gdc_data[gdc_data$P.value <= 0.05,]
+home.dir <- '/home/stephen/Documents/classes/bme/237/final_project'
+setwd(home.dir)
 
-# Get rid of p-values, correlation, RMSE
-gdc_data <- gdc_data[,1:(ncol(gdc_data)-3)]
+# load expression and meta data
+exp.gdc = readRDS(paste0(home.dir, "/data/exp_gdc_recoded_aggr.rds"))
+exp.kal = readRDS(paste0(home.dir, "/data/exp_kal_recoded_aggr.rds"))
+meta.data = readRDS(paste0(home.dir, "/data/meta_data.rds"))
+
+# load cibersort results
+cb.gdc <- read.table(
+  file = "./analysis/cb_gdc_results.txt",
+  sep = "\t",
+  header = TRUE
+)
+
+cb.kal <- read.table(
+  file = "./analysis/cb_kal_results.txt",
+  sep = "\t",
+  header = TRUE
+)
+
+cb.gdc.val <- cb.gdc[,1:(ncol(cb.gdc)-3)]
+cb.kal.val <- cb.kal[,1:(ncol(cb.kal)-3)]
+
+cb.gdc.val.sig <- cb.gdc.val[cb.gdc$P.value <= 0.05, ]
+
+
+gdc_data <- cb.gdc.val.sig
+
+
+
 
 # 1. Determine k using fviz_nbclust() from factoextra (Within Sum Squares)
-# set.seed(123)
+set.seed(123)
 
-# fviz_nbclust(gdc_data, kmeans, method = "wss") + geom_vline(xintercept = 3, linetype = 2)
+fviz_nbclust(gdc_data, kmeans, method = "wss") + geom_vline(xintercept = 3, linetype = 2)
 
 # Output recommends 3 clusters
 
 
 # 2. Determine k using average silhouette width
 
-# library(cluster)
-# sil <- rep(0, 20)
-# repeat k-means for 1:20 and extract silhouette:
-# for(i in 2:20){
-#   k1to20 <- kmeans(gdc_data, centers = i, nstart = 25, iter.max = 20)
-#   ss <- silhouette(k1to20$cluster, dist(gdc_data))
-#   sil[i] <- mean(ss[, 3])
-# }
+library(cluster)
+sil <- rep(0, 20)
+#repeat k-means for 1:20 and extract silhouette:
+for(i in 2:20){
+   k1to20 <- kmeans(gdc_data, centers = i, nstart = 25, iter.max = 20)
+   ss <- silhouette(k1to20$cluster, dist(gdc_data))
+   sil[i] <- mean(ss[, 3])
+ }
 
 # Plot the  average silhouette width
-# plot(1:20, sil, type = "b", pch = 19, xlab = "Number of clusters k", ylab="Average silhouette width")
-# abline(v = which.max(sil), lty = 2)
+plot(1:20, sil, type = "b", pch = 19, xlab = "Number of clusters k", ylab="Average silhouette width")
+abline(v = which.max(sil), lty = 2)
 
 # Output: Recommends 1 clusters
 
 # 3. Calinsky Criterion
 
 # install.packages("vegan")
-# library(vegan)
+#library(vegan)
 # fit <- cascadeKM(gdc_data, 1, 20, iter = 100)
 # plot(fit, sortg = TRUE, grpmts.plot = TRUE)
 
@@ -64,12 +89,13 @@ gdc_data <- gdc_data[,1:(ncol(gdc_data)-3)]
 
 # Compute k-means with k = 3
 # set.seed(123)
+
+gdc_data <- gdc_data[ , which(apply(gdc_data, 2, var) != 0)]
+
+
 km_gdc <- kmeans(gdc_data, 3, nstart = 25)
 print(km_gdc)
-
 fviz_cluster(km_gdc, data = gdc_data)
-# Error in prcomp.default(data, scale = FALSE, center = FALSE) : 
-#   cannot rescale a constant/zero column to unit variance
 
 
 
--- src/cluster_run.R
+++ src/cluster_run.R
@@ -1,41 +1,120 @@
+require(data.table)
+require(dplyr)
+require(plyr)
+library(ggplot2)
+
+library(igraph)
+library(psych) # cor2dist
+
+
+home.dir <- '/home/stephen/Documents/classes/bme/237/final_project'
+setwd(home.dir)
+
+# load expression and meta data
+exp.gdc = readRDS(paste0(home.dir, "/data/exp_gdc_recoded_aggr.rds"))
+exp.kal = readRDS(paste0(home.dir, "/data/exp_kal_recoded_aggr.rds"))
+meta.data = readRDS(paste0(home.dir, "/data/meta_data.rds"))
+
+# load cibersort results
+cb.gdc <- read.table(
+  file = "./analysis/cb_gdc_results.txt",
+  sep = "\t",
+  header = TRUE
+)
+
+cb.kal <- read.table(
+  file = "./analysis/cb_kal_results.txt",
+  sep = "\t",
+  header = TRUE
+)
+
+# remove info columns
+cb.gdc.val <- cb.gdc[,1:(ncol(cb.gdc)-3)]
+cb.kal.val <- cb.kal[,1:(ncol(cb.kal)-3)]
+
+# filter by p-val
+cb.gdc.val.sig <- cb.gdc.val[cb.gdc$P.value <= 0.05, ]
+cb.kal.val.sig <- cb.kal.val[cb.kal$P.value <= 0.05, ]
+
+# filter cell type by variability
+#   want cell types that a
+#tmp <- apply(cb.gdc.val.sig.joint, 2, function(x) {sum(x)} )
+#tmp[tmp < 1.0]
+
+# filter for prevelance (if too low %, then remove)
+# filter for variance (if too little variance, then remove)
+#   - want to find/cluster on what is diff... so just remove those that aren't different????
+
+
+
+# intersecting samples
+overlap.samples <- intersect(rownames(cb.kal.val.sig), rownames(cb.gdc.val.sig))
+print(paste(length(overlap.samples),  '/', nrow(cb.kal.val.sig)))
+
+cb.gdc.val.sig.joint <- cb.gdc.val.sig[rownames(cb.gdc.val.sig) %in% overlap.samples, ]
+cb.kal.val.sig.joint <- cb.kal.val.sig[rownames(cb.kal.val.sig) %in% overlap.samples, ]
+
+# transpose and re-name columns
+cb.gdc.val.sig.joint.t <- transpose(cb.gdc.val.sig.joint)
+rownames(cb.gdc.val.sig.joint.t) <- colnames(cb.gdc.val.sig.joint)
+colnames(cb.gdc.val.sig.joint.t) <- rownames(cb.gdc.val.sig.joint)
+
+cb.kal.val.sig.joint.t <- transpose(cb.kal.val.sig.joint)
+rownames(cb.kal.val.sig.joint.t) <- colnames(cb.kal.val.sig.joint)
+colnames(cb.kal.val.sig.joint.t) <- rownames(cb.kal.val.sig.joint)
+
+# sort samples by sample name
+cb.gdc.val.sig.joint.t <- cb.gdc.val.sig.joint.t[,order(colnames(cb.gdc.val.sig.joint.t))]
+cb.kal.val.sig.joint.t <- cb.kal.val.sig.joint.t[,order(colnames(cb.kal.val.sig.joint.t))]
+
+
+
+# cluster data
+# https://psych-networks.com/r-tutorial-identify-communities-items-networks/
+
+data <- cb.gdc.val.sig.joint.t
+data <- data[ , which(apply(data, 2, var) != 0)]
+
+correlationmatrix = cor(data)
+distancematrix <- cor2dist(correlationmatrix)
+DM1 <- as.matrix(distancematrix)
+
+# test correlations
+#View(apply(correlationmatrix, 1, function(x) {sum(x)/ length(x)}))
+
+
+
+## Zero out connections where there is low (absolute) correlation
+## Keeps connection for cor ~ -1
+## You may wish to choose a different threshhold
+DM1[abs(correlationmatrix) < 0.33] = 0  # threshold 0.33
+#DM1[abs(correlationmatrix) < 0.05] = 0  # threshold 0.33
+#DM1[correlationmatrix < 0.33] = 0      # no abs (filters out negative correlation)
+
+G1 <- graph.adjacency(DM1, mode = "undirected", weighted = TRUE, diag = TRUE)
+vcount(G1)
+ecount(G1)
+
+clusterlouvain <- cluster_louvain(G1)
+plot(G1, vertex.color=rainbow(12, alpha=0.6)[clusterlouvain$membership])
+
+num.clusters <- length(unique(clusterlouvain$membership))
+num.clusters
+
+
+
+# explore LM22 matrix
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+lm22 <- read.table(
+  file = "./bin/cibersort/LM_not_22.txt",
+  sep = ",",
+  header = TRUE
+)
 
 
 
